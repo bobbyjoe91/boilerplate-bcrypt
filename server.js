c785609823ecfadf6f886874ee2b7c9bc91a4d49
bobbyjonathan <bobby.jonathan@dexagroup.com>
2023-03-02 10:47:01 +0700

    add other example

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   })
 });
 
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //END_ASYNC
 
